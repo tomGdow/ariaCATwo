a7907817b925c9183a6e757e62003d12dda0f4fe
Thomas Dowling <thomas@thomas-Vostro-2520.(none)>
2014-04-11 14:05:20 +0100

    added ajax request

---
+++ js/xmlHTTP.js
@@ -0,0 +1,33 @@
+var xmlFileName = "xmlfile.xml";
+
+if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest();
+}
+else {
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+xmlhttp.open("GET", xmlFileName, false);
+xmlhttp.send();
+xmlDoc = xmlhttp.responseXML;
+
+x = xmlDoc.getElementsByTagName("person");
+
+function displayUser() {
+
+    var display_fname = (
+        x[0].getElementsByTagName("fname")[0].childNodes[0].nodeValue);
+    var display_lname = (
+        x[0].getElementsByTagName("lname")[0].childNodes[0].nodeValue);
+
+    document.getElementById("fname").innerHTML = display_fname;
+    document.getElementById("lname").innerHTML = display_lname;
+}
+
+//add Event Handlers
+document.getElementById('buttonId').addEventListener("click", function  () {
+
+    displayUser();
+
+}, false);
+
+
